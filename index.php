845cbe6711b8e3519701182d531e98377701e17a
ernixl <ernixl@yahoo.com>
2017-03-16 11:31:13 +0000

    yes

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPEhtml>
 <html>
- <head>
+<head>
 </head>
 <body>
- <p>
- <?php
- echo "Hello World today <br>";
- echo "My very first php trial, simply trying to make it right is it right?";
- ?>
+<p>
+    <?php
+    echo "Hello World today <br>";
+    echo "My very first php trial, simply trying to make it right is it right?";
+    ?>
 </p>
 </body>
 </html>
