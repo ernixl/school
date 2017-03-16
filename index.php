1a5b922e5deaf82320eafb43ca14989a87fd08d5
ernixl <ernixl@yahoo.com>
2017-03-16 11:24:41 +0000

    yes

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ernes
- * Date: 16/03/2017
- * Time: 11:07
- */
-
-echo "My very first php trial, simply trying to make it right";
+<!DOCTYPEhtml>
+<html>
+ <head>
+</head>
+<body>
+ <p>
+ <?php
+ echo "Hello World";
+ echo "My very first php trial, simply trying to make it right";
+ ?>
+</p>
+</body>
+</html>
