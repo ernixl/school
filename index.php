f7eb9e52647ef781f88b26e222f0ad91f30faa1a
ernixl <ernixl@yahoo.com>
2017-03-16 12:11:07 +0000

    yes

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 </p>
 <p>
     <?php
-$age = "30";
+$age = "17";
 if ($age >= "16" and $age<= "18")print "buy specs";
     else if ($age > "18" and $age <= "21") print "buy mugs";
     else if ($age>"21")print "buy sausage rolls";
