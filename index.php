7e7db1690f98cfbc869e182c957db6bf60baed7c
ernixl <ernixl@yahoo.com>
2017-03-16 11:36:02 +0000

    yes

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPEhtml>
 <html>
 <head>
+    The exercise just started
 </head>
 <body>
 <p>
     <?php
     echo "Good morning <br>";
-    echo "My very first php trial, simply trying to make it right as expected";
+    echo "My very first php trial, simply trying to make it right as expected and I am excited it seems to be working";
+    ?>
+</p>
+
+<p>
+    <?php
+
     ?>
 </p>
 </body>
