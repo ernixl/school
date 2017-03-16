3121804b2bc9234194119df7de75529942204c03
ernixl <ernixl@yahoo.com>
2017-03-16 12:10:23 +0000

    yes

--- index.php
+++ index.php
@@ -10,21 +10,18 @@
     echo "My very first php trial, simply trying to make it right as expected and I am excited it seems to be working";
     ?>
 </p>
-
 <p>
     <?php
     echo "Hello, world!<br>";
     echo "Hello," . " " . "world" . "!";
     ?>
 </p>
-
 <p>
     <?php
     echo "5*7<br>";
     echo 5 * 8;
     ?>
 </p>
-
 <p>
     <?php
     $myname = "ernest";
@@ -32,7 +29,6 @@
     echo "my name is not " . $myname . " and I am" . $myage . "years old";
     ?>
 </p>
-
 <p>
     <?php
     $name = "simon";
@@ -40,7 +36,14 @@
         print "who are you!";
     else print "I know you";
     ?>
-
+</p>
+<p>
+    <?php
+$age = "30";
+if ($age >= "16" and $age<= "18")print "buy specs";
+    else if ($age > "18" and $age <= "21") print "buy mugs";
+    else if ($age>"21")print "buy sausage rolls";
+    ?>
 </p>
 </body>
 </html>
